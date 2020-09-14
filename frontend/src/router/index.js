c36a032ec9b812fd1603c6f80dc7fdbb72739dc6
박태록 <tetrapod3dev@gmail.com>
2020-09-14 14:59:27 +0000

    [TR] router / Update vue 라우터 구축

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,46 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+// Home
+import Home from "../views/Home.vue";
+
+// Account
+import LoginView from "../views/accounts/LoginView.vue";
+import RegisterView from "../views/accounts/RegisterView.vue";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/accounts/login",
+    name: "LoginView",
+    component: LoginView,
+  },
+  {
+    path: "/accounts/register",
+    name: "RegisterView",
+    component: RegisterView,
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
