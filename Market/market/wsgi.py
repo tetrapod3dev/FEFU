51328f662fcd4c2703c0e1705e77501535351db4
unknown <chriskwon96@naver.com>
2020-09-14 17:22:22 +0900

    [KE] feature/products / change Django project name to Market

--- Market/market/wsgi.py
+++ Market/market/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'products.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market.settings')
 
 application = get_wsgi_application()
