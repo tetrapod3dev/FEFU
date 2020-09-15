954e533929d260cde3b1084c4c0af50a10bb2414
unknown <chriskwon96@naver.com>
2020-09-15 15:19:11 +0900

    [KE] feature/products | FIX product models.py

--- Market/product/models.py
+++ Market/product/models.py
@@ -1,13 +1,62 @@
 from django.db import models
 
-# Create your models here.
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete
-# Feel free to rename the models, but don't rename db_table values or field names.
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=80)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
 
 class BadgeInfo(models.Model):
     no = models.AutoField(primary_key=True)
@@ -85,6 +134,43 @@ class DailyQuestInfo(models.Model):
         db_table = 'daily_quest_info'
 
 
+class DjangoAdminLog(models.Model):
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class MainCategoryInfo(models.Model):
     no = models.AutoField(primary_key=True)
     main_category_name = models.CharField(max_length=45, blank=True, null=True)
@@ -141,6 +227,17 @@ class ProofMessageInfo(models.Model):
         db_table = 'proof_message_info'
 
 
+class PurchaseDetails(models.Model):
+    no = models.AutoField(primary_key=True)
+    seller = models.ForeignKey('User', models.DO_NOTHING, db_column='seller', related_name='seller')
+    buyer = models.ForeignKey('User', models.DO_NOTHING, db_column='buyer', related_name='buyer')
+    product_no = models.ForeignKey(ProductInfo, models.DO_NOTHING, db_column='product_no')
+
+    class Meta:
+        managed = False
+        db_table = 'purchase_details'
+
+
 class QuestBadge(models.Model):
     no = models.AutoField(primary_key=True)
     badge_no = models.ForeignKey(BadgeInfo, models.DO_NOTHING, db_column='badge_no')
@@ -149,30 +246,6 @@ class QuestBadge(models.Model):
     class Meta:
         managed = False
         db_table = 'quest_badge'
-
-
-class QuestionCommentInfo(models.Model):
-    no = models.AutoField(primary_key=True)
-    content = models.CharField(max_length=100)
-    writer = models.ForeignKey('User', models.DO_NOTHING, db_column='writer')
-    question_no = models.ForeignKey('QuestionInfo', models.DO_NOTHING, db_column='question_no')
-
-    class Meta:
-        managed = False
-        db_table = 'question_comment_info'
-
-
-class QuestionInfo(models.Model):
-    no = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=100)
-    content = models.CharField(max_length=100)
-    complete = models.IntegerField(blank=True, null=True)
-    writer = models.ForeignKey('User', models.DO_NOTHING, db_column='writer')
-    campaign_no = models.ForeignKey(CampaignInfo, models.DO_NOTHING, db_column='campaign_no')
-
-    class Meta:
-        managed = False
-        db_table = 'question_info'
 
 
 class SubCategoryInfo(models.Model):
@@ -210,24 +283,14 @@ class User(models.Model):
         db_table = 'user'
 
 
-class UserAutority(models.Model):
+class UserAuthority(models.Model):
     no = models.AutoField(primary_key=True)
     user_no = models.ForeignKey(User, models.DO_NOTHING, db_column='user_no')
     authority = models.CharField(max_length=45)
 
     class Meta:
         managed = False
-        db_table = 'user_autority'
-
-
-class UserBadgeFavor(models.Model):
-    no = models.AutoField(primary_key=True)
-    user_no = models.ForeignKey(User, models.DO_NOTHING, db_column='user_no')
-    badge_no = models.ForeignKey(BadgeInfo, models.DO_NOTHING, db_column='badge_no')
-
-    class Meta:
-        managed = False
-        db_table = 'user_badge_favor'
+        db_table = 'user_authority'
 
 
 class UserQuestComplete(models.Model):
