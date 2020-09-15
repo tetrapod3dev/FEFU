ec3f9f8d9fd0189ce357cfa644a59c6452d9a94d
박태록 <tetrapod3dev@gmail.com>
2020-09-15 14:31:07 +0900

    [TR] layout / ADD 기본 페이지 레이아웃 구축

---
+++ frontend/src/api/api.js
@@ -0,0 +1,11 @@
+export default {
+  URL: "http://localhost:8080",
+  // URL: "http://j3a402.p.ssafy.io",
+  ROUTES: {
+    accounts: {
+      URL: "user/",
+      login: "login",
+      signup: "join",
+    },
+  },
+};
