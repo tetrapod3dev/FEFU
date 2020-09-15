e165986343f22eb83e1523b8531e8833e61c49ac
unknown <chriskwon96@naver.com>
2020-09-15 15:21:12 +0900

    [KE] feature/products | ADD sold function and category function

--- Market/product/views.py
+++ Market/product/views.py
@@ -3,8 +3,8 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .models import ProductInfo, MainCategoryInfo, SubCategoryInfo
-from .serializers import ProductSerializer
+from .models import ProductInfo, MainCategoryInfo, SubCategoryInfo, PurchaseDetails
+from .serializers import ProductSerializer, PurchaseDetailsSerializer
 
 
 # Create your views here.
@@ -45,17 +45,59 @@ class ProductViewSet(viewsets.ModelViewSet):
     @action(detail=True)
     def sold(self, request):
         # 요청 보내는 사람과 판매글 작성자가 같은지 확인
+        # 토큰  값
+        # request.META["HTTP_AUTHORIZATION"]
+        # request.META.get("Authorization")
         
-        
+        user_id = request.META["user_id"]
+        product_no = request.data["product_no"]
+        buyer = request.data["buyer"]
 
-        pass
+        product = self.queryset.get(pk=product_no)
+        seller = product.writer
+        status = product.status
+
+        if user_id == seller:
+            if status == 1: #팔 -> 안팔
+                product_serializer = ProductSerializer(product, data={"status": 0}, partial=True)
+
+                purchase_detail = PurchaseDetails.objects.get(product_no=product_no)
+                purchase_detail.delete()
+
+            else: # 안팔 -> 팔
+                product_serializer = ProductSerializer(product, data={"status": 1}, partial=True)
+                purchase_serializer = PurchaseDetailsSerializer(data={"seller": seller, "buyer": buyer, "product_no": product_no})
+                if purchase_serializer.is_valid():
+                    purchase_serializer.save()
+                else:
+                    return HTTPResponse("형식 또는 입력이 옳지 않습니다.")
+
+            if product_serializer.is_valid():
+                product_serializer.save()
+
+                return HTTPResponse(status=200)
+        
+        return HTTPResponse("본인의 판매글만 변경할 수 있습니다.")
+
+
 
     @action(detail=True)
     def categories(self, request):
-        main_category = MainCategoryInfo.objects.all()
-        sub_category = SubCategoryInfo.objects.all()
+        main_categories = MainCategoryInfo.objects.all()
+        sub_categories = SubCategoryInfo.objects.all()
+
+        total_categories = dict()
+
+        for main_cat in main_categories:
+            total_categories[main_cat.main_category_name] = []
+
+        for sub_cat in sub_categories:
+            if sub_cat.main_category_no == main_cat.no:
+                total_categories[main_cat.main_category_name].append(sub_cat.sub_category_name)
+
+        return Response(total_categories)
         
-        #적절한 형태로 변환해서 return
+        
 
 
 
