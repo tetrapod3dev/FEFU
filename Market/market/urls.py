9742b0ba9ce0149f65ce0c9bc2df34863bdbc141
unknown <chriskwon96@naver.com>
2020-09-15 23:47:22 +0900

    [KE] feature/products | FIX router urls

--- Market/market/urls.py
+++ Market/market/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 
 from product.views import ProductViewSet
@@ -27,4 +27,5 @@ router.register("products", ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
