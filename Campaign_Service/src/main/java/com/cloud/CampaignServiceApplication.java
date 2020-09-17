51bbb4e021da0aac337849db08e43e9d0ac3f524
gustn16113 <gustn16113@naver.com>
2020-09-17 17:08:29 +0900

    [HS] campaign-service | INIT 프로젝트 구성

---
+++ Campaign_Service/src/main/java/com/cloud/CampaignServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignServiceApplication.class, args);
+	}
+
+}
