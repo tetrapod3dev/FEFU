ceaf1934284df3334331ac427f72c49a30dc43a6
Jiyoon-park <soundpjy1205@gmail.com>
2020-09-23 16:50:35 +0900

    [JY] market / ADD search

---
+++ frontend/src/components/market/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input
+      type="text"
+      class="custom-search"
+      placeholder="🔍 검색어를 입력하세요"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.custom-search {
+  border: 2px solid black;
+  border-radius: 10px;
+  padding: 7px 10px;
+  margin: 10px 0 20px 0;
+  text-align: start;
+  width: 100%;
+  font-family: "NanumBarunpen";
+}
+</style>
