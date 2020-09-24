7a98b5d630dc3554e10be06607ddd84a2a717c78
Donghyeok-Lee <lee33843@gmail.com>
2020-09-24 18:54:41 +0900

    [DH] CF, MF / ADD IBCF, MF, Hybrid

--- Data_Analysis/read_dummy_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-DUMP_FILE = os.path.join("dummy_data.pkl")
-
-dataframes = pd.read_pickle(DUMP_FILE)
-
-print(dataframes['users'])
-print(dataframes['view_counts'])
