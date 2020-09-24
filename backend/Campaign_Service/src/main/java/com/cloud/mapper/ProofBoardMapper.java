185834221c954968203204c44d2c3aaf32eaf778
gustn16113 <gustn16113@naver.com>
2020-09-24 17:46:24 +0900

    [HS] campaign-service | ADD 공식 캠페인 인증률 구현

--- backend/Campaign_Service/src/main/java/com/cloud/mapper/ProofBoardMapper.java
+++ backend/Campaign_Service/src/main/java/com/cloud/mapper/ProofBoardMapper.java
@@ -1,6 +1,7 @@
 package com.cloud.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -13,4 +14,7 @@ public interface ProofBoardMapper {
 	public int totalCount(int no, boolean flag);
 	public int insertProof(ProofBoardDto dto);
 	public int updateProof(int no);
+	public int findHeadcount(int no);
+	public int findTodayPassOfficial(int no);
+	public Map<String, Object> findWeekPassOfficial(int no);
 }
--- backend/Campaign_Service/src/main/java/com/cloud/service/ProofBoardServiceImpl.java
+++ backend/Campaign_Service/src/main/java/com/cloud/service/ProofBoardServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cloud.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,5 +33,27 @@ public class ProofBoardServiceImpl implements ProofBoardService{
 	@Override
 	public int totalCount(int no, boolean flag) {
 		return proofMapper.totalCount(no, flag);
+	}
+	
+	
+	@Override
+	public int findTodayPassOfficial(int no) {
+		int headcount = proofMapper.findHeadcount(no);
+		int pass = proofMapper.findTodayPassOfficial(no);
+		return (pass / headcount) * 100;
+	}
+	
+	@Override
+	public Map<String, Object> findWeekPassOfficial(int no) {
+		int headcount = proofMapper.findHeadcount(no);
+		Map<String, Object> map = proofMapper.findWeekPassOfficial(no);
+		if(map == null) {
+			return null;
+		}
+		for(String key : map.keySet()) {
+			int res = ((int)map.get(key) / headcount) * 100;
+			map.replace(key, res);
+		}
+		return map;
 	}
 }
