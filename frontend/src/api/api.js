a05865ef618961696530d8ec58c109fc1de72fae
Taerok <sdf7575@naver.com>
2020-09-24 14:09:13 +0900

    [TR] login, signup / UPDATE 로그인 회원가입 API 연결

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,12 +1,12 @@
 export default {
   // URL: "http://localhost:8080",
   // URL: "http://j3a402.p.ssafy.io",
-  URL: "http://j3a402.p.ssafy.io:8800",
+  URL: "http://j3a402.p.ssafy.io:8801",
   ROUTES: {
     accounts: {
-      URL: "/auth",
-      login: "/login",
-      signup: "/join",
+      URL: "",
+      login: "/auth/login",
+      signup: "/users",
     },
     products: {
       URL: "/product",
