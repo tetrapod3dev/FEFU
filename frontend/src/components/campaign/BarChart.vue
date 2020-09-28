ae01d6d5d084f332f501bfb705119c938ddd9bcf
Jiyoon-park <soundpjy1205@gmail.com>
2020-09-28 16:29:47 +0900

    [JY] campaign / ADD 개인캠페인 인증페이지

---
+++ frontend/src/components/campaign/BarChart.vue
@@ -0,0 +1,24 @@
+<script>
+import { Bar } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  mounted() {
+    this.renderChart(
+      {
+        labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        datasets: [
+          {
+            label: "일주일의 기록",
+            backgroundColor: " #37cdc2",
+            data: [40, 20, 12, 39, 14, 20, 39],
+            borderColor: "rgba(1, 116, 188, 0.50)",
+            hoverBackgroundColor: "#079992",
+          },
+        ],
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );
+  },
+};
+</script>
