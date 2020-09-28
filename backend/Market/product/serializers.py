de238db1f63aebc3a4af7e1cdf6b84a1206dbd2b
unknown <chriskwon96@naver.com>
2020-09-28 23:56:40 +0900

    [KE] feature/products | ADD ProductDetailSerializer

--- backend/Market/product/serializers.py
+++ backend/Market/product/serializers.py
@@ -21,7 +21,12 @@ class ViewDetailsSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = (["no"])
 
-
-
+class ProductDetailSerializer(serializers.Serializer):
+    main_category_name = serializers.CharField(max_length=45, read_only=True)
+    medium_category_name = serializers.CharField(max_length=45, read_only=True)
+    sub_category_name = serializers.Charfield(max_length=45, read_only=True)
+    class Meta:
+        model = ProductInfo
+        fields = ["no", "writer", "title", "content", "contact", "price", "photo", "eco_point", "status", "reg_time"]
 
 
