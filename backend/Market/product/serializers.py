7d46033b625a308aff6fcd167bf4b888e2102094
unknown <chriskwon96@naver.com>
2020-09-29 00:09:12 +0900

    [KE] feature/products | FIX typo

--- backend/Market/product/serializers.py
+++ backend/Market/product/serializers.py
@@ -24,7 +24,7 @@ class ViewDetailsSerializer(serializers.ModelSerializer):
 class ProductDetailSerializer(serializers.Serializer):
     main_category_name = serializers.CharField(max_length=45, read_only=True)
     medium_category_name = serializers.CharField(max_length=45, read_only=True)
-    sub_category_name = serializers.Charfield(max_length=45, read_only=True)
+    sub_category_name = serializers.CharField(max_length=45, read_only=True)
     class Meta:
         model = ProductInfo
         fields = ["no", "writer", "title", "content", "contact", "price", "photo", "eco_point", "status", "reg_time"]
