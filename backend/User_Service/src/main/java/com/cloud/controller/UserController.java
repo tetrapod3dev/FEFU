89b2e17c001da8adf70a3e9394c2ebeb02d2628c
gustn16113 <gustn16113@naver.com>
2020-09-29 14:01:17 +0900

    [HS] user-service | ADD 유저 정보, 비밀번호 변경 추가

--- backend/User_Service/src/main/java/com/cloud/controller/UserController.java
+++ backend/User_Service/src/main/java/com/cloud/controller/UserController.java
@@ -1,18 +1,23 @@
 package com.cloud.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cloud.domain.UserDto;
 import com.cloud.service.UserService;
 
 import io.swagger.annotations.ApiOperation;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 public class UserController {
@@ -55,5 +60,27 @@ public class UserController {
 			return new ResponseEntity<String>("사용가능", HttpStatus.OK);
 		}
 		return new ResponseEntity<String>("사용불가능", HttpStatus.OK);
+	}
+	
+	@ApiOperation(value= "유저 정보 변경 (nickname, age, gender, profileImage, introduce) 데이터 누락 주의")
+	@PatchMapping(value = "/")
+	public ResponseEntity<String> modifyUser(@ApiIgnore @RequestHeader("X-USERNAME") String username, @RequestBody UserDto dto){
+		dto.setUsername(username);
+		int res = userService.updateUser(dto);
+		if(res == 0) {
+			return new ResponseEntity<String>("변경실패", HttpStatus.OK);
+		}
+		return new ResponseEntity<String>("변경완료", HttpStatus.OK);
+	}
+	
+	@ApiOperation(value= "비밀번호 변경")
+	@PatchMapping(value = "/password")
+	public ResponseEntity<String> modifyPassword(@ApiIgnore @RequestHeader("X-USERNAME") String username, @RequestBody Map<String, String> map){
+		String password = map.get("password");
+		int res = userService.changePassword(password, username);
+		if(res == 0) {
+			return new ResponseEntity<String>("변경실패", HttpStatus.OK);
+		}
+		return new ResponseEntity<String>("변경완료", HttpStatus.OK);
 	}
 }
