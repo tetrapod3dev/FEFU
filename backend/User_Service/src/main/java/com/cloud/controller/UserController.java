bea0100a3bc08f50876cd6d95012444ebb4fce8f
gustn16113 <gustn16113@naver.com>
2020-09-29 11:03:40 +0900

    [HS] user-service | ADD Nickname 중복 체크

--- backend/User_Service/src/main/java/com/cloud/controller/UserController.java
+++ backend/User_Service/src/main/java/com/cloud/controller/UserController.java
@@ -12,12 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cloud.domain.UserDto;
 import com.cloud.service.UserService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
+	@ApiOperation(value= "회원가입")
 	@PostMapping(value = "/")
 	public ResponseEntity<String> signup(@RequestBody UserDto dto){
 		int res = userService.userRegistier(dto);
@@ -27,9 +30,20 @@ public class UserController {
 		return new ResponseEntity<String>("등록성공", HttpStatus.OK);
 	}
 	
+	@ApiOperation(value= "이메일 중복 체크")
 	@GetMapping(value = "/check-email/{email}")
 	public ResponseEntity<String> checkEmail(@PathVariable("email") String username){
 		int res = userService.checkByUsername(username);
+		if(res == 0) {
+			return new ResponseEntity<String>("사용가능", HttpStatus.OK);
+		}
+		return new ResponseEntity<String>("사용불가능", HttpStatus.OK);
+	}
+	
+	@ApiOperation(value= "닉네임 중복 체크")
+	@GetMapping(value = "/check-nickname/{nickname}")
+	public ResponseEntity<String> checkNickname(@PathVariable("nickname") String nickname){
+		int res = userService.checkByNickname(nickname);
 		if(res == 0) {
 			return new ResponseEntity<String>("사용가능", HttpStatus.OK);
 		}
--- backend/User_Service/src/main/java/com/cloud/service/UserService.java
+++ backend/User_Service/src/main/java/com/cloud/service/UserService.java
@@ -6,5 +6,6 @@ public interface UserService {
 	
 	public int userRegistier(UserDto dto);
 	public int checkByUsername(String usernmae);
+	public int checkByNickname(String nickname);
 	public int insertUserRole(String usernmae);
 }
