8957cd9ffd5f2fb2c0236fc5a57bc6dbcf27ada1
Jiyoon-park <soundpjy1205@gmail.com>
2020-10-01 21:01:38 +0900

    [JY] campaign / UPDATE campaign detail list 반응형

--- frontend/src/views/campaigns/CampaignDetailInfo.vue
+++ frontend/src/views/campaigns/CampaignDetailInfo.vue
@@ -1,86 +1,48 @@
 <template>
   <div class="campaign-info d-flex flex-column">
-    <v-list class="campaign-info-list">
+    <v-list class="campaign-info-list text-left">
       <v-row>
-        <v-list-item class="campaign-info-list-item">
-          <v-col cols="2">
-            <v-list-item-content>
-              <v-list-item-title>인증 미션</v-list-item-title>
-            </v-list-item-content>
-          </v-col>
-          <v-col cols="10">
-            <v-list-item-content class="text-left">
-              <v-list-item-title>{{
-                campaignTypeInfo.mission
-              }}</v-list-item-title>
-            </v-list-item-content>
-          </v-col>
-        </v-list-item>
+        <v-col cols="12" md="2">
+          <span class="campaign-info-title">인증 미션</span>
+        </v-col>
+        <v-col cols="12" md="10">
+          <span>{{ campaignTypeInfo.mission }}</span>
+        </v-col>
       </v-row>
       <v-row>
-        <v-list-item class="campaign-info-list-item">
-          <v-col cols="2">
-            <v-list-item-content>
-              <v-list-item-title>인증 방법</v-list-item-title>
-            </v-list-item-content>
-          </v-col>
-          <v-col cols="10">
-            <v-list-item-content class="text-left">
-              <v-list-item-title>{{
-                campaignTypeInfo.authProcess
-              }}</v-list-item-title>
-            </v-list-item-content>
-          </v-col>
-        </v-list-item>
+        <v-col cols="12" md="2">
+          <span class="campaign-info-title">인증 방법</span>
+        </v-col>
+        <v-col cols="12" md="10">
+          <span>{{ campaignTypeInfo.authProcess }}</span>
+        </v-col>
       </v-row>
       <v-row>
-        <v-list-item class="campaign-info-list-item">
-          <v-col cols="2">
-            <v-list-item-content>
-              <v-list-item-title>인증 시간</v-list-item-title>
-            </v-list-item-content>
-          </v-col>
-          <v-col cols="10">
-            <v-list-item-content class="text-left">
-              <v-list-item-title
-                >{{ campaignTypeInfo.authStartTime }} -
-                {{ campaignTypeInfo.authEndTime }}</v-list-item-title
-              >
-            </v-list-item-content>
-          </v-col>
-        </v-list-item>
+        <v-col cols="12" md="2">
+          <span class="campaign-info-title">인증 시간</span>
+        </v-col>
+        <v-col cols="12" md="10">
+          <span>
+            {{ campaignTypeInfo.authStartTime }} -
+            {{ campaignTypeInfo.authEndTime }}</span
+          >
+        </v-col>
       </v-row>
       <v-row>
-        <v-list-item class="campaign-info-list-item">
-          <v-col cols="2">
-            <v-list-item-content>
-              <v-list-item-title>목표 인원</v-list-item-title>
-            </v-list-item-content>
-          </v-col>
-          <v-col cols="10">
-            <v-list-item-content class="text-left">
-              <v-list-item-title
-                >{{ campaignTypeInfo.headcount }}명
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-col>
-        </v-list-item>
+        <v-col cols="12" md="2">
+          <span class="campaign-info-title">목표 인원</span>
+        </v-col>
+        <v-col cols="12" md="10">
+          <span> {{ campaignTypeInfo.headcount }}명 </span>
+        </v-col>
       </v-row>
       <v-row>
-        <v-list-item class="campaign-info-list-item">
-          <v-col cols="2">
-            <v-list-item-content>
-              <v-list-item-title>멤버 조건</v-list-item-title>
-            </v-list-item-content>
-          </v-col>
-          <v-col cols="10">
-            <v-list-item-content class="text-left">
-              <v-list-item-title>{{
-                campaignTypeInfo.requirement
-              }}</v-list-item-title>
-            </v-list-item-content>
-          </v-col>
-        </v-list-item>
+        <v-col cols="12" md="2">
+          <span class="campaign-info-title">멤버 조건</span>
+        </v-col>
+        <v-col cols="12" md="10">
+          <span>{{ campaignTypeInfo.requirement }}</span>
+        </v-col>
       </v-row>
     </v-list>
 
@@ -140,14 +102,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.campaign-welcome {
-  border: 2px solid black;
-  border-radius: 5px;
-  padding: 10px 20px;
-  margin: 10px 0;
-  text-align: start;
-}
-
 .campaign-title {
   font-size: 1.5rem;
   font-family: "NanumBarunpen";
@@ -163,8 +117,13 @@ export default {
 
 .campaign-info-list {
   margin: 10px 0;
+  padding: 0 20px;
   background-color: rgba(55, 205, 194, 0.09);
   border-radius: 10px;
+  font-family: "NanumBarunpen";
+}
+
+.campaign-info-title {
   font-family: "S-CoreDream-7ExtraBold";
 }
 
