b1391f4d0d8ba6daba199887edbd18e9534dd81e
unknown <chriskwon96@naver.com>
2020-10-05 17:52:45 +0900

    [KE] feature/fix-market | FIX pagination

--- frontend/src/components/core/Pagination.vue
+++ frontend/src/components/core/Pagination.vue
@@ -1,7 +1,10 @@
 <template>
   <nav aria-label="Page navigation">
     <ul class="pagination">
-      <li @click="movePage($event)" class="page-item">
+      <li v-if="prev" @click="movePage($event)" class="page-item">
+        <p class="page-link" aria-label="Previous">&laquo;</p>
+      </li>
+      <li v-else class="page-item-disabled">
         <p class="page-link" aria-label="Previous">&laquo;</p>
       </li>
       <li @click="movePage($event)" v-if="curPage - 1 > 0" class="page-item">
@@ -13,7 +16,10 @@
       <li @click="movePage($event)" v-if="curPage < maxPage" class="page-item">
         <p class="page-link">{{ curPage + 1 }}</p>
       </li>
-      <li @click="movePage($event)" class="page-item">
+      <li v-if="next" @click="movePage($event)" class="page-item">
+        <p class="page-link" aria-label="Next">&raquo;</p>
+      </li>
+      <li v-else class="page-item-disabled">
         <p class="page-link" aria-label="Next">&raquo;</p>
       </li>
     </ul>
@@ -30,6 +36,12 @@ export default {
     maxPage: {
       type: Number,
     },
+    next: {
+        type: Boolean
+    },
+    prev: {
+        type: Boolean
+    }
   },
   methods: {
     movePage(event) {
@@ -60,6 +72,18 @@ export default {
     border-radius: 15px;
     height: 45px;
   }
+  p {
+    margin: 0 1.15em;
+    display: inline-block;
+  }
+}
+.page-item-disabled {
+  font-family: "Nunito", "NanumSquareRound", sans-serif;
+  font-size: 20px;
+  display: inline;
+  padding: 2px;
+  margin: 5px;
+  color: #c9c9c9;
   p {
     margin: 0 1.15em;
     display: inline-block;
